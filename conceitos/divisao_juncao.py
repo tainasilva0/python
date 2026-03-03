d4cec1c94dc183b95c935a6d28a69418c1cd388d
Tainá Vitória <taina2391@gmail.com>
2026-03-03 20:46:49 -0300

    update

---
+++ conceitos/divisao_juncao.py
@@ -0,0 +1,5 @@
+exemplo = 'Conceitos avançados em Python'
+print(exemplo)
+
+print(exemplo.split()) # divide e cria uma lista com cada palavra da variável
+print('-'.join(exemplo)) # junta as palavras e usar o separador
