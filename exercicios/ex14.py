05763479e22a93f9b984c061fce88e41e7f31823
Tainá Vitória <taina2391@gmail.com>
2026-04-07 21:13:02 -0300

    update

--- exercicios/ex14.py
+++ exercicios/ex14.py
@@ -1,7 +1,18 @@
-print('Pense em um número entre 0 e 5')
+from random import randint
+from time import sleep
+
+print('-=' * 22)
+print('Bora jogar?')
+print('Vou pensar em um número entre 0 e 10.')
+print('-=' * 22)
+
+num = int(input(('Descubra qual número escolhi: ')))
+computador = randint(0,10)
+
 print('Pensando...')
-num = int(input(('Descubra qual número escolhi:')))
-if num >= 0 or num <= 5:
+sleep(2)
+
+if num == computador:
     print('Acertou!')
 else:
     print('Errou!')
--- exercicios/ex16.py
+++ exercicios/ex16.py
@@ -1,2 +1,10 @@
-for i in range(0,3):
-    n1 = int(input('Digite o {i + 1} número'.format(i)))
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+n3 = int(input('Digite o terceiro número: '))
+
+if n1 > n2 > n3:
+    print('O maior número é o {}'.format(n1))
+elif n3 > n2 > n1:
+    print('O maior número é o {}'.format(n3))
+else:
+    print('O maior número é o {}'.format(n2))
